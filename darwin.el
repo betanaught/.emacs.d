891e4a7f3c197d7188a867c74bede8ebe7cc101a
betanaught <brendan.wakefield@gmail.com>
2022-02-14 19:50:59 -0800

    correct ')' typos

--- darwin.el
+++ darwin.el
@@ -26,7 +26,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages '(markdown-mode elpy)))
- '(package-selected-packages '(ess markdown-mode elpy)))
+ '(package-selected-packages '(ess markdown-mode elpy))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
