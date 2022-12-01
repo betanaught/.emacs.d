d91db2013c3ea4048c56fcb133859b58517105ee
bwakefield <bwakefield@usgs.gov>
2022-12-01 09:55:20 -0800

    feat: update sh-mode autoindent - 2

--- darwin.el
+++ darwin.el
@@ -17,8 +17,12 @@
 
 (setq backup-directory-alist `(("." . "~/.emacs.d/emacs.saves")))
 (setq backup-by-copying t)
-;(setq backup-by-copying-when-linked t)
-
+;;(setq backup-by-copying-when-linked t)
+;; sh-mode
+;; sh-mode
+(setq sh-basic-offset 2)
+(setq sh-indentation 2)
+(setq smie-indent-basic 2)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
